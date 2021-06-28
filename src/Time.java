0f8148710af4f1c6ac3656ee03980527afdc8545
Afiq Nazrie <57625126+mangkoran@users.noreply.github.com>
2021-06-28 12:30:39 +0800

    Add Time.java

---
+++ src/Time.java
@@ -0,0 +1,23 @@
+import java.time.LocalTime;
+
+public class Time {
+    // private String time;
+    private String hall;
+    private LocalTime time;
+
+    public String getHall() {
+        return hall;
+    }
+
+    public void setHall(String hall) {
+        this.hall = hall;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+}
