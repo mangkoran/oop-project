2bcbb221c6797632c6f218293b196ef7ab3bff50
leonna <67146445+leonnahart@users.noreply.github.com>
2021-06-29 15:10:31 +0800

    add super size in Fries.java

--- Fries.java
+++ Fries.java
@@ -1,16 +1,10 @@
 public class Fries extends Food {
-    private char size;
 
     public Fries(String name, char size, int price) {
-        super(name, price);
-        this.size = size;
+        super(name, size, price);
     }
 
     public String description() {
         return "Thinly sliced potatoes that are deep-fried till they're crisp on all sides and then sprinkled with salt";
-    }
-
-    public char getSize() {
-        return size;
     }
 }
