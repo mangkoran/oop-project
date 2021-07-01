a3f76347a9ac022fc3aa2ab295525dfb8f9ba336
Afiq Nazrie <57625126+mangkoran@users.noreply.github.com>
2021-07-01 20:21:00 +0800

    Add Theatre.java

---
+++ src/Theatre.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import movie.*;
+
+public class Theatre {
+    private String name;
+    ArrayList<Movie> movies;
+
+    public Theatre(String name, ArrayList<Movie> movies) {
+        this.name = name;
+        this.movies = movies;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public Movie getMovie(int i) {
+        return movies.get(i);
+    }
+
+    public void setMovie(int i, Movie m) {
+        this.movies.set(i, m);
+    }
+}
